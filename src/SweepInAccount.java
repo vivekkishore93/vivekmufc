d1f16611aceb6e6313089b408bf4c78161539c16
vivekkishore93 <vivek.kishore93@gmail.com>
2013-12-24 20:33:39 +0530

    2nd commit

--- src/SweepInAccount.java
+++ src/SweepInAccount.java
@@ -11,7 +11,7 @@ public class SweepInAccount extends BankAccount{
 	
 	private final double rateOfInterest = 8.25;
 	private final double timePeriod = 0.5;
-	
+	private int a ;
 	public double calculateInterest(double principle) {
 		double interest = (principle * rateOfInterest * timePeriod)/100;
 		return interest;
